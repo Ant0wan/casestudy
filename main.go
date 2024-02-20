2b01f7af6994e218b18b0d336d9e569a49c8989c
Antoine Barthelemy <antoine@linux.com>
2024-02-20 17:09:45 +0100

    feat(part1): run but does not work properly

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 // main.go
 // Learned scraping from https://brightdata.com/blog/how-tos/web-scraping-go
+// then read gocolly colly documentation https://go-colly.org/docs/examples/basic/
+// doc https://developer.mozilla.org/fr/docs/Web/API/Document/querySelector
 // thousand ways to improve this:
 // focus on: scraping with lib for maintainance, using argparse for further cli
 // option/arg completion, flexibility in format output as a backend tool could
 // be useful to parse it via http or cli for the different services around.
+// paging to take into account
+// 3 structs: top, athing, bottom
 
 package main
 
 import (
-	"github.com/gocolly/colly"
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	collector := colly.NewCollector()
-	collector.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+	// Instantiate default collector
+	// Not sure configuration is good here
+	c := colly.NewCollector()
 
-	collector.Visit("https://news.ycombinator.com/")
-	fmt.Println("Hello, World!")
-	// scraping logic...
+	// On every a element which has href attribute call callback
+	//c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c.OnHTML(".athing", func(e *colly.HTMLElement) {
+		id := e.Attr("id")
+		// Print link
+		fmt.Println(id)
+	})
+
+	c.Visit("https://news.ycombinator.com/")
 }
